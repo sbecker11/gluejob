37220f17198964a83d1769410d858245f6fc3de7
Shawn Becker <sbecker11@me.com>
2024-02-13 22:14:59 -0700

    updates

--- process.py
+++ process.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 # Access the properties from the .env file
 INPUT_CSV_FILE = os.getenv("INPUT_CSV_FILE")
-OUTPUT_FOLDER = os.getenv("OUTPUT_FOLDER")
+OUTPUT_PARQUET_FOLDER = os.getenv("OUTPUT_PARQUET_FOLDER")
 
 
 def process():
@@ -23,16 +23,19 @@ def process():
     output_path = "assets/output/random_orders.parquet"
 
     # Read CSV file into DataFrame
-    df = spark.read.csv(input_path, header=True)
+    df = spark.read.csv(INPUT_CSV_FILE, header=True)
 
     # Create a new timestamp column from the datetime column
     df = df.withColumn("timestamp", to_timestamp(col("datetime")))
 
     # Write DataFrame as Parquet files partitioned by customer_id and timestamp
     df.write.partitionBy("user_id", "timestamp") \
-        .parquet(OUTPUT_FOLDER, mode="overwrite")
+        .parquet(OUTPUT_PARQUET_FOLDER, mode="overwrite")
 
     # Stop SparkSession
     spark.stop()
 
-    print(f"Parquet files have been created in the '{OUTPUT_FOLDER}' folder.")
+    print(f"Parquet files have been created in the '{OUTPUT_PARQUET_FOLDER}' folder.")
+
+if __file__ != "__main__":
+    process()
